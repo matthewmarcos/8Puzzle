4a0e630dff9663761f42b655e6777e17e53cfbb4
Matthew <matthewmaros94@gmail.com>
2016-02-16 18:26:32 +0800

    Finished randomization of initial state

--- Puzzle.java
+++ Puzzle.java
@@ -1,6 +1,48 @@
+import java.util.*;
+
 public class Puzzle {
 
-    public static void main (String[] args) {
+    private static State currentState;
 
+    public static void main (String[] args) {
+        currentState = Puzzle.createRandomState();
+        currentState.printMe();
+    }
+
+    public static State createRandomState() {
+        int [][] tempBoard = new int[3][3];
+        Random random = new Random();
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 3 ; j++) {
+                tempBoard[i][j] = 0;
+            }
+        }
+        for(int i = 1 ; i < 9 ; i++) {
+            int tempI;
+            int tempJ;
+            while(!isExisting(tempBoard, i)) {
+                tempI = random.nextInt(3);
+                tempJ = random.nextInt(3);
+                if(tempBoard[tempI][tempJ] == 0) {
+                    tempBoard[tempI][tempJ] = i;
+                    break;
+                }
+            }
+        }
+
+        return new State(null, tempBoard, 0);
+    }
+
+    private static boolean isExisting(int[][] toTest, int x) {
+        // Returns true if number x exists in toTest[][]
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 3 ; j++) {
+                if(toTest[i][j] == x) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
--- State.java
+++ State.java
@@ -5,36 +5,60 @@ public class State {
     private int h; //remaining distance
     private int f; // g+h
 
-    public State (State parent, int[][] board, int g, int h) {
+    public State (State parent, int[][] board, int g) {
         this.parent = parent;
         this.g = g;
-        this.h = h;
-        this.f = g + h;
+        // this.h = h;
         for(int i = 0 ; i < 3 ; i++) {
             for(int j = 0 ; j < 3 ; j++) {
                 this.board[i][j] = board[i][j];
             }
         }
+        this.h = this.getManhattanDistance();
+        this.f = this.g + this.h;
 
     }
 
     public State doAction(Action a) {
+        int i = a.getI();
+        int j = a.getJ();
         // Check if action is legit
+        if()
+
         return this; //Temporary
     }
 
     private Action getEmpty() {
-        // Returns the empty button in its board
+        // Returns the empty button in its board and returns it as an action
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 3 ; j++) {
+                if(board[i][j] == 0) {
+                    return new Action(i, j);
+                }
+            }
+        }
+        return new Action(0, 0);
     }
 
     public void printMe() {
         // Prints the entire state. For debugging.
-
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 3 ; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 
     private int getManhattanDistance () {
         // returns manhattan distance of this state
-        return 0;
+        int manhattanDistance = 0;
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 3 ; j++) {
+
+            }
+        }
+        return manhattanDistance;
     }
 
     public boolean goalTest() {
@@ -42,7 +66,7 @@ public class State {
         for(int i = 0 ; i < 3 ; i++) {
             for(int j = 0 ; j < 3 ; j++) {
                 if(this.board[i][j] != num) {
-                    System.out.println("False");
+                    System.out.println("Not yet solved!");
                     return false;
                 }
                 num++;
